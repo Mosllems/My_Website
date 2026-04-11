362bd204e1f06beda239cc85efbc273972477708
moslem <moslemamiri82@gmail.com>
2026-04-11 13:44:24 +0330

    add Home template

---
+++ pages/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'pages' # we must write the apps'name here, because once the project get bigger calling the urls will be harder, and by app name we can distinguish them from eachother
+
+
+urlpatterns = [
+    path('', views.HomePageView.as_view(), name='home'),
+    # path('contact/', views.contact, name='contact'),
+    # path('about/', views.about, name='about'),
+
+]
