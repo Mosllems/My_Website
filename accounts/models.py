d68129881d1105c74560b5ca9af0fb06efaf20e3
moslem <moslemamiri82@gmail.com>
2026-04-12 14:28:28 +0330

    Update CustomUser

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,12 @@ class CustomUser(AbstractUser):
     # You can add additional fields here if needed
     age = models.DateField(null=True, blank=True)
     phone_number = models.CharField(max_length=15, null=True, blank=True)
+    address = models.TextField(blank=True)
+    website = models.URLField(max_length=250, blank=True) 
+    github = models.URLField(max_length=250, blank=True)
+    linkedin = models.URLField(max_length=250, blank=True)
+    bio = models.TextField(blank=True)
+
 
     def __str__(self):
         return self.username
