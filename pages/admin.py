35bba098235c4a5fa301337c3a0eb1b72b0e52b0
moslem <moslemamiri82@gmail.com>
2026-04-13 09:39:56 +0330

    Update Model in Pages app

--- pages/admin.py
+++ pages/admin.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
-from pages.models import Category, Skill
+from pages.models import Category, Skill, Interest
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ["name"]
+    list_display = ["name", "updated_at"]
 
 
 class SkillAdmin(admin.ModelAdmin):
-    list_display = ["name", "category", "icon_type"]
+    list_display = ["name", "category", "icon_type", "updated_at"]
     list_filter = ["category"]
     list_per_page = 15
     search_fields = ["name"]
 
 
+class InterestAdmin(admin.ModelAdmin):
+    list_display = ["name", "updated_at"]
+    list_per_page = 15
+    search_fields = ["name"]
+
+
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Skill, SkillAdmin)
+admin.site.register(Interest, InterestAdmin)
--- pages/models.py
+++ pages/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=150, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     class Meta:
         verbose_name = "Category"
@@ -27,6 +29,14 @@ class Skill(models.Model):
     name = models.CharField(max_length=150)
     icon_class = models.CharField(max_length=150, blank=True)
     icon_type = models.CharField(max_length=150, choices=ICON_TYPES, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+
+
+class Interest(models.Model):
+    name = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
